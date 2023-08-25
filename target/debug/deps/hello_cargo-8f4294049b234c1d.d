15dbbd0433ea59ba1dbe9e7d8935ac9e631d4a47
MrSeekzz <MrSeekz@drakessens.io>
2023-08-25 12:28:25 +0200

    change things

--- target/debug/deps/hello_cargo-8f4294049b234c1d.d
+++ target/debug/deps/hello_cargo-8f4294049b234c1d.d
@@ -1,5 +1,5 @@
-/Users/vaporeta/Documentos/Proyectos Rust/El comienzo/hello-cargo/target/debug/deps/hello_cargo-8f4294049b234c1d.rmeta: src/main.rs
+/Users/vaporeta/Downloads/pruebaRust/target/debug/deps/hello_cargo-8f4294049b234c1d.rmeta: src/main.rs
 
-/Users/vaporeta/Documentos/Proyectos Rust/El comienzo/hello-cargo/target/debug/deps/hello_cargo-8f4294049b234c1d.d: src/main.rs
+/Users/vaporeta/Downloads/pruebaRust/target/debug/deps/hello_cargo-8f4294049b234c1d.d: src/main.rs
 
 src/main.rs:
